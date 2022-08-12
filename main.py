faf5f02d9ff0f6a16fec1f380ede41be9a750a14
FrancoMRamos <91027520+FrancoMRamos@users.noreply.github.com>
2022-08-12 00:22:29 -0500

    Agregando las tuberias

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame       # Importamos la librería Pygame
 from pygame.locals import *     # Con esto estamos todos los módulos locales de Pygame
+import random       # Importamos la librería Random
 
 pygame.init()
 
@@ -18,6 +19,9 @@ posicion_suelo = 0
 suelo_velocidad = 4
 volar = False
 game_over = False
+tuberia_espaciado = 150         #px
+tuberia_frecuencia = 1500        #milisegundos
+tuberia_ultima = pygame.time.get_ticks() - tuberia_frecuencia
 
 # Cargamos las imagenes
 bg = pygame.image.load('img/bg.png')
@@ -75,7 +79,26 @@ class Bird(pygame.sprite.Sprite):
         else:
             self.image = pygame.transform.rotate(self.images[self.index], -90)
 
+# Realizamos una clase para las tuberias
+class Tuberia(pygame.sprite.Sprite):
+    def __init__(self, x, y, posicion):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load('img/pipe.png')
+        self.rect = self.image.get_rect()
+        # position 1 es desde arriba y -1 desde el suelo
+        if posicion == 1:
+            self.image = pygame.transform.flip(self.image, False, True)
+            self.rect.bottomleft = [x, y - int(tuberia_espaciado/2)] 
+        if posicion == -1:
+            self.rect.topleft = [x, y + int(tuberia_espaciado/2)]
+    
+    def update(self):
+        self.rect.x -= suelo_velocidad
+        if self.rect.right < 0:
+            self.kill()         #eliminacion de las tuberias sobrantes
+
 bird_group = pygame.sprite.Group()
+tuberia_group = pygame.sprite.Group()
 
 flappy = Bird(100, int(alto / 2))
 bird_group.add(flappy)
@@ -90,21 +113,37 @@ while run:
 
     bird_group.draw(ventana)
     bird_group.update()
+    tuberia_group.draw(ventana)
 
     # Agregando el suelo 
     ventana.blit(suelo, (posicion_suelo, 768))
+
+    # Colision con las tuberias
+    if pygame.sprite.groupcollide(bird_group, tuberia_group, False, False) or flappy.rect.top < 0:
+        game_over = True
 
     # Verificacion del personaje en el suelo 
     if flappy.rect.bottom >= 768:
         game_over = True
         volar = False
 
-    if game_over == False:
+    if game_over == False and volar == True:
+        # creacion nuevas tuberias
+        tiempo_actual = pygame.time.get_ticks()
+        if tiempo_actual - tuberia_ultima > tuberia_frecuencia:
+            altura_tuberia = random.randint(-100, 100)
+            btm_tuberia = Tuberia(ancho, int(alto / 2) + altura_tuberia, -1)
+            top_tuberia = Tuberia(ancho, int(alto / 2) + altura_tuberia, 1)
+            tuberia_group.add(btm_tuberia)
+            tuberia_group.add(top_tuberia)
+            tuberia_ultima = tiempo_actual
 
         # Agregando movimiento al suelo  
         posicion_suelo -= suelo_velocidad
         if abs(posicion_suelo) > 35:
             posicion_suelo = 0
+
+        tuberia_group.update()
 
     # Para cerrar el programa, se deberá presionar la X de la ventana de Windows
     for event in pygame.event.get():
